eefda53a73c840a89fe29217eb5e4839ef442457
wang.xiaohu9 <wang.xiaohu9@ztesoft.com>
2020-02-20 18:31:33 +0800

    Initial commit

---
+++ precache-manifest.903d5327844aa89e40753575cd90fd97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988a060baf2c856ebc3531394a115905",
+    "url": "./index.html"
+  },
+  {
+    "revision": "071d3f45d0d275857819",
+    "url": "./static/css/main.26c863f9.chunk.css"
+  },
+  {
+    "revision": "96397b28f24380c8bb71",
+    "url": "./static/js/2.8b4a2335.chunk.js"
+  },
+  {
+    "revision": "071d3f45d0d275857819",
+    "url": "./static/js/main.84b9b9be.chunk.js"
+  },
+  {
+    "revision": "516a1d332b30432ebde9",
+    "url": "./static/js/runtime-main.acf0ac42.js"
+  },
+  {
+    "revision": "edd71a453e67f8ed5fac9333353d32e5",
+    "url": "./static/media/pic.edd71a45.png"
+  }
+]);
